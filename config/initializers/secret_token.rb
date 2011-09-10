91f9258e3b774a3860b6501cfcefeef6567a3165
Kiran Ryali <kiran.ryali@gmail.com>
2011-09-09 20:06:48 -0500

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whiteboard::Application.config.secret_token = '0dce5596da700382d9719de4a9a2a97b3ab3910564f37c02c3160dac0871b5e49afa1c0b73afd4dc06e4999e379b7f7d8e754748dfec1f54646511c4df6b3242'
