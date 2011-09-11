82f7e6ace5c7f381c6769d92fbebc7cef7746deb
Kiran Ryali <kiran.ryali@gmail.com>
2011-09-11 17:29:32 -0500

    Login logic actually makes sense now, woo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  protected
-  attr_accessor :current_user
-
-  def current_user
-    unless session[:user_id]
-      # If there is no session, no user is logged in
-      return nil
-    else
-      # Else, we must have a user, so return it
-      begin
-        @current_user = @current_user || User.find(session[:user_id])
-        return @current_user
-      rescue
-        return nil
-      end
-    end
-  end
-
-  def current_user=(user)
-    @current_user = user 
-    session[:user_id] = user.id
-  end
-
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,44 +1,39 @@
 class UserController < ApplicationController
   def login
-#    render :text => params
-    @user = User.find(params["id"])
-    session[:user_id] = @user.id if @user.id
-    redirect_to(root_path)
-=begin
-    @user = User.find_by_fb_id(params["id"])
-    unless @user
-      @user = User.new
-      @user.first_name = params["first_name"]
-      @user.last_name = params["last_name"]
-      @user.tj
+    begin 
+      @user = User.find(params["userID"])
+    rescue ActiveRecord::RecordNotFound
+      register(params["accessToken"], params["userID"]);
+    ensure
+      current_user = @user
+      session[:user_id] = @user.id if @user.id
+      redirect_to(root_path)
     end
-=end 
   end
 
-  def register
-    @user = User.find(params["userID"])
-    current_user = @user
-    session[:user_id] = @user.id if @user.id
-    if not @user
-      @user = User.new
-      graph = Koala::Facebook::API.new(params["accessToken"])
-      user_data = graph.get_object(params["userID"])
-      friends = graph.get_connections(params["userID"], "friends")
-      friends.each do |friend|
-        Friendship.create(:friend_id => friend["id"], :user_id => params["userID"])
-      end
-      @user.fb_token = params["accessToken"]
-      @user.id = params["userID"]
-      @user.name = user_data["name"]
-      @user.email = user_data["email"]
-      @user.first_name = user_data["first_name"]
-      @user.last_name = user_data["last_name"]
-      @user.link = user_data["link"]
-      @user.gender = user_data["gender"]
-      @user.save
-    end
-    redirect_to(root_path)
+  #
+  # register receives an accessToken and a userID, then
+  # uses koala to retrieve the user's data 
+  # and facebook friends
+  #
+  def register(accessToken, userID)
+    # User wasn't found, register him
+    @user = User.new
+    graph = Koala::Facebook::API.new(accessToken)
+    user_data = graph.get_object(userID)
+    friends = graph.get_connections(userID, "friends")
+    friends.each { |friend| Friendship.create(:friend_id => friend["id"], :user_id => userID) }
+    @user.fb_token = accessToken
+    @user.id = userID
+    @user.name = user_data["name"]
+    @user.email = user_data["email"]
+    @user.first_name = user_data["first_name"]
+    @user.last_name = user_data["last_name"]
+    @user.link = user_data["link"]
+    @user.gender = user_data["gender"]
+    @user.save
   end
+
 
   def logout
     session[:user_id] = nil
