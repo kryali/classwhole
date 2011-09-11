789d8bd0ca80b8300137cefcc3c1435ac269c436
Kiran Ryali <kiran.ryali@gmail.com>
2011-09-11 14:01:23 -0500

    Improved login logic and added jquery, Closes #1

--- app/assets/javascripts/fb_button_init.js
+++ app/assets/javascripts/fb_button_init.js
@@ -1,50 +1,66 @@
-function login_to_rails() {
-  FB.api('/me', function(user) {
-    console.log(user);
-    console.log("posting...");
-    post_to_url('/user/login', user);
-    console.log("posted");
-  });
-}
+/*
+ * fb_button_init.js
+ * File Summary: initializes the facebook sdk and contains login logic via facebook 
+ */
 
-function post_to_url(path, params, method) {
-    method = method || "post"; // Set method to post by default, if not specified.
+$(document).ready(function() { 
 
-    // The rest of this code assumes you are not using a library.
-    // It can be made less wordy if you use one.
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", path);
-    form.setAttribute("authenticity_token", AUTH_TOKEN);
+  /*
+   * This creates a hidden form and posts this data to rails
+   */
+  function post_to_url(path, params, method) {
+      method = method || "post"; // Set method to post by default, if not specified.
 
-    for(var key in params) {
-        var hiddenField = document.createElement("input");
-        hiddenField.setAttribute("type", "hidden");
-        hiddenField.setAttribute("name", key);
-        hiddenField.setAttribute("value", params[key]);
-        hiddenField.setAttribute("authenticity_token", AUTH_TOKEN);
-        form.appendChild(hiddenField);
-    }
-    document.body.appendChild(form);    // Not entirely sure if this is necessary
-    form.submit();
-}
+      // The rest of this code assumes you are not using a library.
+      // It can be made less wordy if you use one.
+      var form = document.createElement("form");
+      form.setAttribute("method", method);
+      form.setAttribute("action", path);
+      form.setAttribute("authenticity_token", AUTH_TOKEN);
 
-FB.init({
-  appId  : '155445894543736',
-  status : true, // check login status
-  cookie : true, // enable cookies to allow the server to access the session
-  xfbml  : true, // parse XFBML
-  oauth  : true // enable OAuth 2.0
-});
-
-FB.Event.subscribe('auth.login', function(response) {
-  console.log(response);
-  post_to_url('user/register', response["authResponse"]);
-});
-
-FB.getLoginStatus(function(response) {
-  if( response.status == "connected" ){
-    //$("#fb-login").css("display","none");
-    login_to_rails();
+      for(var key in params) {
+          var hiddenField = document.createElement("input");
+          hiddenField.setAttribute("type", "hidden");
+          hiddenField.setAttribute("name", key);
+          hiddenField.setAttribute("value", params[key]);
+          hiddenField.setAttribute("authenticity_token", AUTH_TOKEN);
+          form.appendChild(hiddenField);
+      }
+      document.body.appendChild(form);    // Not entirely sure if this is necessary
+      form.submit();
   }
+
+  /* 
+   * FB.init starts the facebook sdk, enabling calls against the facebook api
+   * also, the facebook "sdk" sucks.
+   */
+  FB.init({
+    appId  : '155445894543736',
+    status : true, // check login status
+    cookie : true, // enable cookies to allow the server to access the session
+    xfbml  : true, // parse XFBML
+    oauth  : true // enable OAuth 2.0
+  });
+
+
+  /* When a user clicks a button, */
+  $('#fb-button').click( function(event) {
+
+    /* FB.getLoginStatus allows you to determine if a user is 
+     * logged in and connected to your app.*/
+    FB.getLoginStatus(function(response) {
+      if( response.authResponse ) {
+        /* Log the user in with it's facebook graph data */
+        FB.api('/me', function(user) {
+          post_to_url('/user/login', user);
+        });
+      }
+    });
+
+    /* FB.Event.subscribe('auth.login') gets fired when the user logs in
+     * Register the user by silently posting a form to rails */
+    FB.Event.subscribe('auth.login', function(response) {
+      post_to_url('user/register', response["authResponse"]);
+    });
+  });
 });
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,9 @@
 class UserController < ApplicationController
   def login
-    render :text => params
+#    render :text => params
+    @user = User.find(params["id"])
+    session[:user_id] = @user.id if @user.id
+    redirect_to(root_path)
 =begin
     @user = User.find_by_fb_id(params["id"])
     unless @user
@@ -9,12 +12,13 @@ class UserController < ApplicationController
       @user.last_name = params["last_name"]
       @user.tj
     end
-    {"id"=>"1342020220", "name"=>"Kiran Ryali", "first_name"=>"Kiran", "last_name"=>"Ryali", "link"=>"https://www.facebook.com/kiranryali", "username"=>"kiranryali", "gender"=>"male", "timezone"=>"-5", "locale"=>"en_US", "verified"=>"true", "updated_time"=>"2011-09-07T05:27:25+0000", "controller"=>"user", "action"=>"login"}
 =end 
   end
 
   def register
-    @user = User.find_by_fb_id(params["userID"])
+    @user = User.find(params["userID"])
+    current_user = @user
+    session[:user_id] = @user.id if @user.id
     if not @user
       @user = User.new
       graph = Koala::Facebook::API.new(params["accessToken"])
@@ -32,11 +36,12 @@ class UserController < ApplicationController
       @user.link = user_data["link"]
       @user.gender = user_data["gender"]
       @user.save
-      render :text => "Finished"
     end
-=begin
-{"id"=>"1342020220", "name"=>"Kiran Ryali", "first_name"=>"Kiran", "last_name"=>"Ryali", "link"=>"http://www.facebook.com/kiranryali", "username"=>"kiranryali", "gender"=>"male", "timezone"=>-5, "locale"=>"en_US", "verified"=>true, "updated_time"=>"2011-09-07T05:27:25+0000"}
-"accessToken"=>"AAACNYJFTeXgBACKHmy2tZABsForq2c2H6yYsk77xLuPnD4nzEp5bEMt5yjiuKiVENB3xZB4KchecZA4T7M3ZBdCX5ptZCUZAk5jbUZAquKSQVQpJNDA8uM3", "userID"=>"1342020220", "expiresIn"=>"7185", "signedRequest"=>"F8NbZKlFvsMuFK23B8YsxVszf-_9zlIzePGO1uk_axE.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUUM5OE5BeVU3d25LQ000dURvZTJydTdrTnRUb2NjRDgySzNDQmwzeVk4NUVKOUFyME11b0ZGd3B0MEFFSDZmVlJYam56RzUxVFpOMmpiWHdyU3o2QS0xMHFVUmNHMGhKRFplcV9kV0pMLUw3bUdWX1F6ZmI4cFpzZmxLd2psbzZnd3NreXRFUzlOVWxfSWE4NlVOOEI5c01QQWF1UGhfSWdoTGIxVWdVV1pwUnR5MjRFcnBRYnpjWnRrUWpfVzRHX2MiLCJpc3N1ZWRfYXQiOjEzMTU2MzgwMTUsInVzZXJfaWQiOiIxMzQyMDIwMjIwIn0", "controller"=>"user", "action"=>"register"}
-=end
+    redirect_to(root_path)
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to(root_path)
   end
 end
