1ab81e8a52fb255da8d72da0a61189a9df22721b
Kiran Ryali <kiran.ryali@gmail.com>
2011-09-28 11:35:34 -0500

    helper function to check for conflicts in model

--- app/controllers/scheduler_controller.rb
+++ app/controllers/scheduler_controller.rb
@@ -2,7 +2,27 @@ class SchedulerController < ApplicationController
   def index
   end
 
+  # Description: This function ensures that no two sections are conflicting
+  #   Method: Make sure that sectionb's start and end time is not between sectiona's start and end time
+  def conflict?(sectiona, sectionb)
+  end
+
+  def generate_schedule
+    schedule = []
+    logger.info "START SCHEDULER"
+    current_user.courses.each do |course|
+      logger.info "Course: #{course.to_s}"
+      course.sections.each do |section|
+        logger.info "\tSection: #{section.code}"
+        logger.info "\t\tTime: #{section.start_time} #{section.end_time}"
+        logger.info "\t\tType: #{section.code}"
+      end
+    end
+    logger.info "END SCHEDULER"
+  end
+
   def show
+    generate_schedule
   end
 
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,24 @@
 class Section < ActiveRecord::Base
   belongs_to :course
 #  has_and_belongs_to_many :users
+
+  # Description: This function ensures that no two sections are conflicting
+  #   Method: Make sure that sectionb's start and end time is not between sectiona's start and end time
+  def conflict?(section)
+
+    # Check for day conflicts
+    day_array = days.split("")
+    day_array.each do |day|
+
+      # If we have a day conflict, check for an hour and minutes conflict
+      if( section.days.include?(day) )
+        if (start_time.to_i   >= section.start_time.to_i and start_time.to_i <= section.end_time.to_i) or
+           (  end_time.to_i   >= section.start_time.to_i and   end_time.to_i <= section.end_time.to_i)
+          return true
+        end
+      end
+    end
+
+    return false
+  end
 end
