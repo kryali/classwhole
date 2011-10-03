a6af579c9958b85a297b99864c1b3a871043a0ab
Kiran Ryali <kiran.ryali@gmail.com>
2011-10-02 19:23:20 -0500

    moved to erb

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -14,4 +14,23 @@ module CatalogHelper
     course_list += "]"
   end
 
+  # Description: 
+  #   This helper method returns a javascript formatted array of subjects
+  #   for use with jquery-ui's autocomplete
+  #
+  def subject_list_for_autocomplete
+    subject_list = "["
+    all_subjects do |subject|
+      subject_list+="{label:\"#{subject.to_s}\",title:\"#{subject.title}\",value:\"#{subject.code}\"},\n"
+        subject_list << { label: "#{subject.to_s}",
+                          title: "#{subject.title}",
+                          value: "#{subject.code}" }
+    end
+    subject_list += "]"
+  end
+
+  def all_subjects
+    @all_subjects ||= Subject.all
+  end
+
 end
