8f20a41fc339693c862e2ef8d9eb1f2348e14803
Kiran Ryali <kiran.ryali@gmail.com>
2011-10-05 16:47:25 -0500

    built redis install script for ubuntu and misc optimizations

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,15 +5,25 @@ class Course < ActiveRecord::Base
 #   has_and_belongs_to_many :users
 
   def self.trie(term)
+
+    results_needed = 10
+
     courses = []
-    possible_courses = $redis.smembers("course:#{term}")
+    possible_courses = $redis.smembers("course:#{term.upcase}")
     possible_courses.each do |course_id|
+
+      if results_needed <= 0
+        break
+      end
+
       label = $redis.hget("id:course:#{course_id}", "label")
       title = $redis.hget("id:course:#{course_id}", "title")
       value = $redis.hget("id:course:#{course_id}", "value")
       courses << {       label: label,
                           title: title,
                           value: value }
+
+      results_needed -= 1
     end
     return courses
   end
--- install_redis.sh
+++ install_redis.sh
@@ -2,3 +2,6 @@ wget http://redis.googlecode.com/files/redis-2.2.14.tar.gz
 tar xzf redis-2.2.14.tar.gz
 cd redis-2.2.14
 make
+sudo cp src/redis-server src/redis-cli /usr/bin/
+cd ..
+rm -rf redis-2.2.14*
