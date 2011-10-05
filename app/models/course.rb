7fdc3fd63fd9a49c489a6b52c92605d12f72daab
Kiran Ryali <kiran.ryali@gmail.com>
2011-10-05 03:53:14 -0500

    added redis searching

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,20 @@ class Course < ActiveRecord::Base
   has_many :announcements
 #   has_and_belongs_to_many :users
 
+  def self.trie(term)
+    courses = []
+    possible_courses = $redis.smembers("course:#{term}")
+    possible_courses.each do |course_id|
+      label = $redis.hget("id:course:#{course_id}", "label")
+      title = $redis.hget("id:course:#{course_id}", "title")
+      value = $redis.hget("id:course:#{course_id}", "value")
+      courses << {       label: label,
+                          title: title,
+                          value: value }
+    end
+    return courses
+  end
+
   def to_s
     return subject_code + " " + number.to_s
   end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,15 +1,35 @@
 $redis = Redis.new( :host => 'localhost', :port => 6379 )
 
+$redis.flushdb
+
 # Build the trie for the course autocomplete
 # use subjects for starters
 Subject.all.each do |subject|
   $redis.sadd("subjects", subject.id)
+  $redis.hset("id:subject:#{subject.id}", "label", subject.to_s)
+  $redis.hset("id:subject:#{subject.id}", "title", subject.title)
+  $redis.hset("id:subject:#{subject.id}", "value", subject.code)
   str = subject.code
   size = str.size
 
   current_str = ""
   size.times do |i|
     current_str += str[i]
-    $redis.sadd("subjects:#{current_str}", subject.id)
+    $redis.sadd("subject:#{current_str}", subject.id)
+  end
+end
+
+Course.all.each do |course|
+  $redis.sadd("courses", course.id)
+  $redis.hset("id:course:#{course.id}", "label", course.to_s)
+  $redis.hset("id:course:#{course.id}", "title", course.title)
+  $redis.hset("id:course:#{course.id}", "value", course.to_s)
+  str = course.to_s
+  size = str.size
+
+  current_str = ""
+  size.times do |i|
+    current_str += str[i]
+    $redis.sadd("course:#{current_str}", course.id)
   end
 end
