832931f00e6b6e32fb652b7b5ed7ea6bbf02fc6e
Kiran Ryali <kiran.ryali@gmail.com>
2011-10-05 23:05:16 -0500

    preliminary schedule front-end, speed up seeds

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,6 @@ module Whiteboard
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.time_zone = "Central Time (US & Canada)"
+    # config.time_zone = "Central Time (US & Canada)"
   end
 end
