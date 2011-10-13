c7dd7114e3ba8ac158571ad64c1de664c067f2de
Scott <wilson29@illinois.edu>
2011-10-13 03:29:19 -0500

    session persistent after browser closes...

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Whiteboard::Application.config.session_store :cookie_store, key: '_whiteboard_session'
+Whiteboard::Application.config.session_store :cookie_store, key: '_whiteboard_session', :expire_after => 1.year
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
