0b58e9e585e2edd15bbee92554d0d5aecf9b8a3d
Scott <wilson29@illinois.edu>
2011-10-13 03:33:47 -0500

    sessions lookin bettuh

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
+# expire_after is set to one year, because that's the work around that the internet gave me
+# in order to havea session cookie NOT expire when the browser is closed
 Whiteboard::Application.config.session_store :cookie_store, key: '_whiteboard_session', :expire_after => 1.year
 
 # Use the database for sessions instead of the cookie-based default,
