c341c281180b788bfe67f522aab39d28416524ab
Kiran Ryali <kiran.ryali@gmail.com>
2011-10-17 04:09:41 -0500

    major redesign, twitter bootstrap stuff, etc

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'rails', '3.1.1'
 # gem 'thinking-sphinx', '2.0.5'
 gem 'passenger'
+gem 'dalli'
 
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Whiteboard::Application.initialize!
+
+# For use with dalli/memcached gem
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    Rails.cache.reset if forked
+  end
+end
