dbecb8e52fac4c064099a24227a6a226812d4618
Kiran Ryali <kiran.ryali@gmail.com>
2011-10-19 20:09:40 -0500

    frontend structure refactor

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,10 @@
 module UserHelper
+
+  def current_user_has_courses?
+    unless current_user and not current_user.courses.empty?
+      return false
+    else
+      return true
+    end
+  end
 end
