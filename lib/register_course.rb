c61aa957953e90aac41536d21b8fc4b70f024d7b
¨Jonathan <krng90@gmail.com>
2011-10-27 01:34:29 -0500

    cleaning up scheduler code

--- lib/register_course.rb
+++ lib/register_course.rb
@@ -1,24 +1,31 @@
 class Register_Course
-  attr_accessor :section_configurations
+  attr_accessor :configurations_hash, :configurations_array
   
   def initialize(course)
     @course = course
-    generate
+    generate_hash
+    generate_array
   end
 
-  def generate
-    @section_configurations = {}
+  #creates a hash of all the sections for this course
+  def generate_hash
+    @configurations_hash ||= {}
     @course.sections.each do |section|
       key = configuration_key(section)
-      @section_configurations[key] ||= {}
-      @section_configurations[key][section.section_type] ||= []
-      @section_configurations[key][section.section_type] << section
+      @configurations_hash[key] ||= {}
+      @configurations_hash[key][section.section_type] ||= []
+      @configurations_hash[key][section.section_type] << section
     end
-    @section_configurations = @section_configurations.sort_by{|k,config| config.length}
-    for i in 0...@section_configurations.length
-      @section_configurations[i] = @section_configurations[i][1].sort_by{|k,sections| sections.length}
-      for j in 0...@section_configurations[i].length
-        @section_configurations[i][j] = @section_configurations[i][j][1]
+  end
+
+  #creates an array of all the sections for this course
+  #sorts based on array sizes
+  def generate_array
+    @configurations_array = @configurations_hash.sort_by{|k,config| config.length}
+    for i in 0...@configurations_array.length
+      @configurations_array[i] = @configurations_array[i][1].sort_by{|k,sections| sections.length}
+      for j in 0...@configurations_array[i].length
+        @configurations_array[i][j] = @configurations_array[i][j][1]
       end
     end
   end
@@ -28,7 +35,7 @@ class Register_Course
     key = section.code.at(0)
     #append number to key if it exists at 1 (for mathematica sections B8, X8, etc)
     at1 = section.code.at(1)
-    key << at1 if true if Integer(at1) rescue false
+    key << at1 if (true if Integer(at1) rescue false)
     return key
   end
 end
