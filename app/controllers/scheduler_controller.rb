11810bf953b40b385cb9d889d8fb832f5f6c93b0
Kiran Ryali <kiran.ryali@gmail.com>
2011-10-27 02:32:26 -0500

    added schedule ids function and rearranged logic

--- app/controllers/scheduler_controller.rb
+++ app/controllers/scheduler_controller.rb
@@ -2,19 +2,15 @@ class SchedulerController < ApplicationController
 
   def index
   end
-
-  def generate_schedules
-    scheduler = Scheduler.new(current_user.courses)
-    scheduler.schedule_courses
-    @possible_schedules = scheduler.valid_schedules
-    render 'show'
-  end
     
   def show
   end
 
   def new
-    generate_schedules
+    scheduler = Scheduler.new(current_user.courses)
+    scheduler.schedule_courses
+    @possible_schedules = scheduler.valid_schedules[0,5]
+    render 'show'
   end
 
 end
