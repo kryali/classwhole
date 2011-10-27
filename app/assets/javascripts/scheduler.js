47633dd1f625bc882b2d3fd5ae75ce3fac318344
Kiran Ryali <kiran.ryali@gmail.com>
2011-10-27 02:57:54 -0500

    adding section html generation to js

--- app/assets/javascripts/scheduler.js
+++ app/assets/javascripts/scheduler.js
@@ -1,5 +1,33 @@
 $(function(){
 
+  function draw_section( section, days ) {
+    var start_time = new Date( Date.parse( section['start_time'] ) );
+    va end_time   = new Date( Date.parse( section['end_time'] ) );
+    var day_array = section[ 'days' ].split( "" );
+    for( var i in day_array)  {
+      var day_element = document.createElement("div");    
+      var hour_diff = end_time.getUTCHours() - start_time.getUTCHours();
+      var min_diff = (end_time.getMinutes() - start_time.getMinutes())/60;
+      var width = $("tr").width();
+      if( !width ) width = 149; /* HACK, need to set the description to the width */
+      $(day_element).css("width", width)
+                    .append($('<span/>')
+                            .append(section['code'] )
+                            .addClass('section-code'))
+                    .append($('<span/>')
+                            .append(section['course_subject_code'] + " " + section['course_number'] )
+                            .addClass('course-name'))
+                    .append($('<span/>')
+                            .append(section['section_type'] )
+                            .addClass('section-type label'));
+      day_element.className = "schedule-block";
+      day_element.style.top = ((start_time.getUTCHours() - DAY_START) * block_height) + "px";
+      day_element.style.height = block_height * (hour_diff + min_diff) + "px";
+      days[ day_array[i] ].appendChild( day_element );
+    }
+  }
+
+
   function init() {
     init_events();
   }
@@ -14,15 +42,18 @@ $(function(){
     });
 
     $(".schedule-block").mousedown( function(){
-      $.ajax({
-        type: 'POST',
-        data: { section:1, schedule:[1,2,3], 'test':'fuck cliff in the face'},
-        url:  '/user/scheduler/move_section',
-        dataType: 'json',
-        success: function(data, textStatus, jqXHR ) {
-          console.log("request completed successfully!");
-          console.log(data);
-        },
+      var section = $(this).find(".hidden").text();
+      get_schedule_ids( function(schedule) {
+        $.ajax({
+          type: 'POST',
+          data: { section: section, schedule:schedule},
+          url:  '/user/scheduler/move_section',
+          dataType: 'json',
+          success: function(data, textStatus, jqXHR ) {
+            console.log("request completed successfully!");
+            console.log(data);
+          },
+        });
       });
     });
   }
@@ -77,21 +108,5 @@ $(function(){
   });
 
   init();
-  get_schedule_ids( function(schedule) { 
-    console.log(schedule);
-    console.log(schedule[0]);
-
-    $.ajax({
-      type: 'POST',
-      data: { section: schedule[0], schedule: schedule},
-      url:  '/user/scheduler/move_section',
-      dataType: 'json',
-      success: function(data, textStatus, jqXHR ) {
-        console.log("request completed successfully!");
-        console.log(data);
-      },
-    });
-
-  });
 
 });
