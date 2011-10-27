b79ece0c25e9a58f9d7a5d464a91fd172bb15cf5
Kiran Ryali <kiran.ryali@gmail.com>
2011-10-27 01:32:10 -0500

    drag and drop function stubs written out, slight redirect and css adjustments

--- app/assets/javascripts/scheduler.index.js
+++ app/assets/javascripts/scheduler.index.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   var autocomplete = new Autocomplete();
   autocomplete.input_suggestion = ".autocomplete-suggestion";
   autocomplete.input = "#autocomplete-list";
-  autocomplete.ajax_search_url = "courses/search/auto/subject/";
+  autocomplete.ajax_search_url = "../courses/search/auto/subject/";
   autocomplete.course_select = class_list.add_class_callback;
   autocomplete.init();
   class_list.init();
