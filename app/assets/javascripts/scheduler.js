7c7c6449f14477d0c354db93834ef24f05a107fa
Kiran Ryali <kiran.ryali@gmail.com>
2011-10-28 11:46:20 -0500

    Drag and drop working, aww yeee. need to work out some bugs though :(

--- app/assets/javascripts/scheduler.js
+++ app/assets/javascripts/scheduler.js
@@ -1,49 +1,79 @@
 $(function(){
 
+  var block_height = 74;
+  var added_hours = false;
+
   function init() {
     init_events();
   }
 
   function init_events() {
     $(".schedule-block").mouseover( function(){
-      $(this).find("ul.hidden-data").fadeIn('fast');
+      //$(this).find("ul.hidden-data").fadeIn('fast');
     });
 
     $(".schedule-block").mouseleave( function(){
-      $(this).find("ul.hidden-data").fadeOut('slow');
-    });
-
-    $(".schedule-block").mousedown( function(){
-      var section = $(this).find(".hidden").text();
-      get_schedule_ids( function(schedule) {
-        $.ajax({
-          type: 'POST',
-          data: { section: section, schedule:schedule},
-          url:  '/user/scheduler/move_section',
-          dataType: 'json',
-          success: function(data, textStatus, jqXHR ) {
-            console.log("request completed successfully!");
-            console.log(data);
-          },
-        });
-      });
+      //$(this).find("ul.hidden-data").fadeOut('slow');
     });
   }
 
-  function get_schedule_ids(callback) {
-    $(".schedule-wrapper").each( function(i) {
-      if( $(this).css("display") == "block" ) {
-        var sections = [];
-        var all_section_ids = $(this).find(".schedule-block .hidden")
-        for( var i = 0; i < all_section_ids.size(); i++ ){
-          var current_section_id = all_section_ids[i].innerHTML;
-          if( sections.indexOf(current_section_id) == -1 ) {
-            sections.push(current_section_id);
-          }
+  function add_hours( num_hours ){
+    var schedule = get_current_schedule();
+    schedule.each( function() {
+      var schedule_day = $(this).find(".schedule-day");
+      var add_list = "";
+      for( var i = 0; i < num_hours; i++) {
+        add_list += "<li></li>";
+      }
+      schedule_day.append(add_list);
+      //console.log(schedule_day);
+      var current_height = $(".slides_control").height();
+      $(".slides_control").height(current_height + block_height * num_hours);
+    });
+  }
+
+  function insert_suggestions(data, textStatus, jqXHR) {
+    var schedule = get_current_schedule();
+    $(data).each( function() {
+      var days_s = $(this).attr("days");
+      if( typeof(days_s) != "undefined" ) {
+        var days_a = days_s.split("");
+        for( var i = 0; i < days_a.length; i++) {
+          var schedule_day = schedule.find("." + days_a[i]);
+          $(this).addClass("droppable");
+          var section_hint = $(this).clone().addClass("droppable");
+          section_hint.droppable({
+              accept:      '.schedule-block',
+              hoverClass:  'hover',
+              drop:        handle_drop
+          });
+          schedule_day.append(section_hint);
         }
-        callback(sections);
       }
     });
+  }
+
+  function get_current_schedule() {
+    var schedules = $(".schedule-wrapper");
+    for( var j = 0; j < schedules.size(); j++) {
+      var current = $(schedules[j]);
+      if( current.css("display") == "block" ) {
+        return current;
+      }
+    }
+  }
+
+  function get_schedule_ids() {
+    var schedule = get_current_schedule();
+    var sections = [];
+    var all_section_ids = schedule.find(".schedule-block .hidden");
+    for( var i = 0; i < all_section_ids.size(); i++ ){
+      var current_section_id = all_section_ids[i].innerHTML;
+      if( sections.indexOf(current_section_id) == -1 ) {
+        sections.push(current_section_id);
+      }
+    }
+    return sections;
   }
 
   // Setup the slidejs plugin
@@ -52,31 +82,74 @@ $(function(){
     generatePagination: true
   });
 
+  function remove_droppable( section_id ) { 
+    $(".schedule-block").each( function() {
+      var current_id = $(this).find(".hidden").text(); 
+      if( current_id == section_id ) {
+        $(this).removeClass("droppable");
+      }
+    });
+  }
+  /* This function removes sections with a given section id */
+  function remove_section( section_id ) {
+    get_current_schedule().find(".schedule-block").each( function() {
+      var current_id = $(this).find(".hidden").text(); 
+      if( current_id == section_id ) {
+        $(this).remove();
+      }
+    });
+  }
+
   function handle_drop( event, ui ) {
-    ui.draggable.addClass( 'correct' );
+    var current_course =  $(ui.draggable[0]);
+    remove_section( current_course.find(".hidden").text() );
+    remove_droppable( $(this).find(".hidden").text() );
+    current_course.remove();
     //ui.draggable.draggable( 'disable' );
+    $(this).removeClass("droppable");
+    ui.draggable.addClass( 'correct' );
     $(this).droppable( 'disable' );
     ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
     ui.draggable.draggable( 'option', 'revert', false );
+
+    $(this).draggable({
+      snap:        '.droppable',
+      start:       start_drag_event,
+      stop:        stop_drag_event,
+      revert:      true,
+    });
+
+    stop_drag_event( undefined, undefined );
   }
 
   function start_drag_event( event, ui ) {
-    console.log(ui);
-    //ui.draggable.draggable( 'option', 'revert', true );
+    if( !added_hours ) { 
+      add_hours(4);
+      added_hours = true;
+    }
+    var current_section = $(ui.helper[0]);
+    current_section.draggable( 'option', 'revert', true );
+    var section = current_section.find(".hidden").text();
+    var schedule_ids = get_schedule_ids();
+    console.log(schedule_ids);
+    $.ajax({
+      type: 'POST',
+      data: { section: section, schedule:schedule_ids},
+      url:  '/scheduler/move_section',
+      success: insert_suggestions,
+    });
+  }
+
+  function stop_drag_event( event, ui ) {
+    console.log("Removing droppables");
+    $('.droppable').remove();
   }
 
   $(".schedule-block").draggable({
-    containment: '.slides_container',
-    cursor:      'move',
     snap:        '.droppable',
-    //start:       start_drag_event,
+    start:       start_drag_event,
+    stop:        stop_drag_event,
     revert:      true,
-  });
-
-  $(".droppable").droppable({
-    accept:      '.schedule-block',
-    hoverClass:  'hover',
-    drop:        handle_drop
   });
 
   init();
