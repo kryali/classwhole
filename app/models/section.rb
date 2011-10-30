a9b8ff54fe7bc3f13657794a796be4f02b5406fb
¨Jonathan <krng90@gmail.com>
2011-10-30 15:53:51 -0500

    fixing bug in scheduler closes #33

--- app/models/section.rb
+++ app/models/section.rb
@@ -15,7 +15,7 @@ class Section < ActiveRecord::Base
   def time_conflict?(days, start_time, end_time)
     day_array = days.split("")
     day_array.each do |day|
-      if( days.include?(day) )
+      if( self.days.include?(day) )
         if (self.start_time.to_i   >= start_time.to_i and self.start_time.to_i <= end_time.to_i) or 
            (  self.end_time.to_i   >= start_time.to_i and   self.end_time.to_i <= end_time.to_i)
           return true
