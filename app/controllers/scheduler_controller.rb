3d3fb1cca26cdb223439e7bf1684b77dffb04f89
Kiran Ryali <kiran.ryali@gmail.com>
2011-11-03 20:47:31 -0500

    limiting to 5 schedules

--- app/controllers/scheduler_controller.rb
+++ app/controllers/scheduler_controller.rb
@@ -9,7 +9,7 @@ class SchedulerController < ApplicationController
   def new
     scheduler = Scheduler.new(current_user.courses)
     scheduler.schedule_courses
-    @possible_schedules = scheduler.valid_schedules
+    @possible_schedules = scheduler.valid_schedules[0,5]
     render 'show'
   end
 
