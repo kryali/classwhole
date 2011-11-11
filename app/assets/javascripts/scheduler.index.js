ab68d39be387d0555fb6bc993c045bd4753c8517
Kiran Ryali <kiran.ryali@gmail.com>
2011-11-11 14:51:05 -0600

    fixing js error and removing loggers

--- app/assets/javascripts/scheduler.index.js
+++ app/assets/javascripts/scheduler.index.js
@@ -1,17 +1,9 @@
-function initialize_autocomplete(){
-  var class_list = new ClassList();
-  var autocomplete = new Autocomplete();
-  autocomplete.input_suggestion = ".autocomplete-suggestion";
-  autocomplete.input = "#autocomplete-list";
-  autocomplete.ajax_search_url = "../courses/search/auto/subject/";
-  autocomplete.course_select = class_list.add_class_callback;
-  autocomplete.init();
-  class_list.init();
-}
-
-
 $(document).ready(function(){
-  initialize_autocomplete()
+
+  // Let class_list be available to all functions by declaring it out of function scope
+  var class_list = new ClassList();
+
+  initialize_autocomplete();
   $(".schedule").click( function(event) {
     if(!class_list.has_classes()) {
       pop_alert("error", "No classes selected.");
@@ -19,19 +11,29 @@ $(document).ready(function(){
       return true;
     }
   });
-});
 
-
-/* This refreshes the course list once a user has logged into facebook */
-$(document).bind('logged-in', function(){
-  $(document).unbind('logged-in');
-  $.ajax({
-  	type: 'POST',
- 		url:  '/user/refresh',					
-    success: function( data, textStatus, xqHR){    	
-        $("div.user-course-list").empty();
-        $("div.user-course-list").append( $(data) );
-        initialize_autocomplete();
-    }
+  /* This refreshes the course list once a user has logged into facebook */
+  $(document).bind('logged-in', function(){
+    $(document).unbind('logged-in');
+      $.ajax({
+        type: 'POST',
+        url:  '/user/refresh',					
+        success: function( data, textStatus, xqHR){    	
+          $("div.user-course-list").empty();
+          $("div.user-course-list").append( $(data) );
+          initialize_autocomplete();
+        }
+      });
   });
+
+  function initialize_autocomplete(){
+    var autocomplete = new Autocomplete();
+    autocomplete.input_suggestion = ".autocomplete-suggestion";
+    autocomplete.input = "#autocomplete-list";
+    autocomplete.ajax_search_url = "../courses/search/auto/subject/";
+    autocomplete.course_select = class_list.add_class_callback;
+    autocomplete.init();
+    class_list.init();
+  }
+
 });
