9572e67817b44e95ddae63ee192533024f03b945
Scott <wilson29@illinois.edu>
2011-11-11 03:15:39 -0600

    fixed small cookie bug

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -150,6 +150,7 @@ include ApplicationHelper
  #
 	def add_course_to_cookie(id)
 		if cookies["classes"]
+      logger.info(id)
 			course_id_string = id.to_s			
 			cook = cookies["classes"] # this is used in the next line, so I didn't have to deal with quotes inside a string		
 			cookies["classes"] = { :value => "#{cook}#{course_id_string}|", :expires => 1.day.from_now } 				
