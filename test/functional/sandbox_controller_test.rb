40561c1f29a9dc86c1ffd17f4cdd3c4849c2603a
Kiran Ryali <kiran.ryali@gmail.com>
2012-03-10 08:55:43 -0600

    html5 dragndrop shit

---
+++ test/functional/sandbox_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SandboxControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/helpers/sandbox_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class SandboxHelperTest < ActionView::TestCase
+end
