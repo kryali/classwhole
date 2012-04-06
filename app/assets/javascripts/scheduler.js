c13e7848be69214fa52fa7b07fe95669e5e1dc68
root <1upgreenshroom@gmail.com>
2012-04-06 16:45:28 -0500

    scheduler

--- app/assets/javascripts/scheduler.js
+++ app/assets/javascripts/scheduler.js
@@ -76,6 +76,37 @@ $(function(){
     });
   }
 
+  function init_tabs(tabList){
+      tabListItems = tabList.childNodes;
+      for ( var i = 0; i < tabListItems.length; i++ ) {
+        if ( tabListItems[i].nodeName == "LI" ) {
+          var tabLink = getFirstChildWithTagName( tabListItems[i], 'A' );
+          var id = getHash( tabLink.getAttribute('href') );
+          tabLinks[id] = tabLink;
+          contentDivs[id] = document.getElementById( id );
+        }
+      }
+
+      // Assign onclick events to the tab links, and
+      // highlight the first tab
+      var i = 0;
+
+      for ( var id in tabLinks ) {
+        tabLinks[id].onclick = showTab;
+        tabLinks[id].onfocus = function() { this.blur() };
+        if ( i == 0 ) tabLinks[id].className = 'selected';
+        i++;
+      }
+
+      // Hide all content divs except the first
+      var i = 0;
+
+      for ( var id in contentDivs ) {
+        if ( i != 0 ) contentDivs[id].className = 'tabContent hide';
+        i++;
+      }
+}
+
   function init_tooltips() {
     $(".register-schedule").tipsy({ gravity: 'n', opacity: .9});
     $(".schedule-block .enrollment").tipsy({ gravity: 's', opacity: .9});
@@ -297,35 +328,11 @@ $(function(){
   }
 
   init = function() {
-      // Grab the tab links and content divs from the page
-      var tabListItems = document.getElementById('tabs').childNodes;
-      for ( var i = 0; i < tabListItems.length; i++ ) {
-        if ( tabListItems[i].nodeName == "LI" ) {
-          var tabLink = getFirstChildWithTagName( tabListItems[i], 'A' );
-          var id = getHash( tabLink.getAttribute('href') );
-          tabLinks[id] = tabLink;
-          contentDivs[id] = document.getElementById( id );
-        }
-      }
-
-      // Assign onclick events to the tab links, and
-      // highlight the first tab
-      var i = 0;
-
-      for ( var id in tabLinks ) {
-        tabLinks[id].onclick = showTab;
-        tabLinks[id].onfocus = function() { this.blur() };
-        if ( i == 0 ) tabLinks[id].className = 'selected';
-        i++;
-      }
-
-      // Hide all content divs except the first
-      var i = 0;
-
-      for ( var id in contentDivs ) {
-        if ( i != 0 ) contentDivs[id].className = 'tabContent hide';
-        i++;
-      }
+    //tab shit for course page
+    var tabList = document.getElementById('tabs');
+    if(tabList != null){
+      init_tabs(tabList);
+    }
     // Setup the slidejs plugin
     //$("#slides").slides(options.slides);
 
